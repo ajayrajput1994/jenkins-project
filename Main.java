204ecbb6298b14f8cb6ce8f2d89143bce7028e84
ajayrajput1994 <55807531+ajayrajput1994@users.noreply.github.com>
2023-12-25 18:50:38 +0530

    initial commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String args[]){
+        System.out.println("this is java main program !");
+        System.out.println("create for jenkins !");
+        System.out.println("this is start now !");
+    }
+}
